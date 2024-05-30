ea208b2ffae1946313996fe030cd78ab9d2fa31b
ianotee <oteeian500@gmail.com>
2024-05-30 09:49:06 +0000

    fourth commit

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,26 +1,17 @@
 export default class Airport {
   constructor(name, code) {
+    if (typeof name !== 'string') {
+      throw new TypeError('name must be a string');
+    }
+    if (typeof code !== 'string') {
+      throw new TypeError('code must be a string');
+    }
+
     this._name = name;
     this._code = code;
   }
 
-  get name() {
-    return this._name;
-  }
-
-  set name(value) {
-    this._name = value;
-  }
-
-  get code() {
-    return this._code;
-  }
-
-  set code(value) {
-    this._code = value;
-  }
-
   toString() {
-    return `[object ${this.code}]`;
+    return `[object ${this._code}]`;
   }
 }
